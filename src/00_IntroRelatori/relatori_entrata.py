131919076d815a9cedfefbae571ca2d4a7203ee7
silentShulk <marcostrada258@gmail.com>
2026-06-14 23:38:55 +0200

    Added Intros

--- src/00_IntroRelatori/relatori_entrata.py
+++ src/00_IntroRelatori/relatori_entrata.py
@@ -11,9 +11,9 @@ class RelatoriEntrata(Scene):
             bg = ImageMobject(f"../assets/imgs/Relatori/{r}/bg.png")\
                 .scale_to_fit_height(config.frame_height)\
                 .scale_to_fit_width(config.frame_width)
-            first = ImageMobject(f"../assets/imgs/Relatori/{r}/{r}.png").scale(0.2)\
+            first = ImageMobject(f"../assets/imgs/Relatori/{r}/{r}_destra.png").scale(0.2)\
                 .to_corner(DR).shift(DOWN*0.5)
-            second = ImageMobject(f"../assets/imgs/Relatori/{r}/{r}2.png").scale(0.2)\
+            second = ImageMobject(f"../assets/imgs/Relatori/{r}/{r}2_destra.png").scale(0.2)\
                 .next_to(first, LEFT)
 
             self.add(bg)
